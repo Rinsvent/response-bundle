9de88229311ba2576b8b3daffd7e5d1e46ad50c1
Rinsvent <rinsvent007@gmail.com>
2022-04-02 11:16:27 +0700

    Проинтегрировал новую версию библиотеки

---
+++ src/Attribute/ResponseSchema.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Rinsvent\ResponseBundle\Attribute;
+
+#[\Attribute(\Attribute::IS_REPEATABLE)]
+class ResponseSchema
+{
+    public function __construct(
+        public string $schemaClass,
+    ) {
+    }
+}
